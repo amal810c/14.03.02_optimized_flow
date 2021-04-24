740d1415646fe10ca633ceca23ae2af9fa90a38c
vivimortensen <vivi4531@stud.kea.dk>
2021-04-24 14:18:51 +0200

    lorte lort

--- main.js
+++ main.js
@@ -1,79 +1,132 @@
 "use strict";
 
 import "./style.scss";
-/* import { headers } from "./settings";
- */
 
-
+window.addEventListener("DOMContentLoaded", init); 
 
 const endpoint = "https://frontend2021-7beb.restdb.io/rest/ezone"; 
 const apiKey = "60740be1f592f7113340f013";
+const lort = {​​​​​​​fullname: "Anton Hansen",
+              gamertag: "anton_anden",
+              email: "anton@anden.dk",
+              date_of_birth: "1994/02/03",
+              password: "453218654565",
+              game_type: "FPS",
+              game_title: "CS",
+              game_hours: "8",
+              game_time_of_day: "10:00am - 14:00pm",
+              // improve_areas: ["sleep"],
+              interested_areas: "stress"}​​​​​​​;
 
 
-window.addEventListener("load", (e) =>{​​​​​​​
-    document.querySelector("button.add-new").addEventListener("click", () =>{​​​​​​​
-        const data = {​​​​​​​
-            fullname: "Anton Hansen",
-            gamertag: "anton_anden",
-            email: "anton@anden.dk",
-            date_of_birth: "1994/02/03",
-            password: "453218654565",
-            game_type: "FPS",
-            game_title: "CS",
-            game_hours: "8",
-            game_time_of_day: "10:00am - 14:00pm",
-            improve_areas: ["sleep"],
-            interested_areas: "stress",
-          }​​​​​​​;
-        post(data);
-    }​​​​​​​);
-}​​​​​​​); 
+
+function init(){
+  console.log("hul"); 
+
+  post(lort); 
+}
 
 function post(data){
-    const postData = JSON.stringify(data);
-    fetch(endpoint, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "x-apikey": apiKey,
-        "cache-control": "no-cache"
-      },
-      body: postData
-    })
-      .then((res) => res.json())
-      .then((data) => showHero(data));
-  }
-
-
-
-
-/* 
-window.addEventListener("load", post(data));
-
-post(data);
-
-function post(data) {
-  const data = {
-    fullname: "Anton Hansen",
-    gamertag: "anton_anden",
-    email: "anton@anden.dk",
-    date_of_birth: "1994/02/03",
-    password: "453218654565",
-    game_type: "FPS",
-    game_title: "CS",
-    game_hours: "8",
-    game_time_of_day: "10:00am - 14:00pm",
-    improve_areas: "sleep",
-    interested_areas: "stress",
-  };
-
-  const postData = JSON.stringify(data);
-  fetch("https://frontend2021-7beb.restdb.io/rest/ezone", {
-    method: "post",
-    headers: headers,
-    body: postData,
+  console.log(data); 
+  const postData = JSON.stringify(data); 
+  fetch(endpoint, {
+    method : "POST", 
+    headers : {"Content-Type": "application/json; charset=utf-8", 
+  "x-apikey" : apiKey, 
+"cache-control" : "no-cache", 
+    },
   })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  .then((res) => res.json())
+  .then((data) => console.log(data));  
 }
- */
+
+
+// /* import { headers } from "./settings";
+//  */
+
+//window.addEventListener("DOMContentLoaded", start); 
+
+// function start(){
+//   console.log("hallo");
+
+
+//     document.getElementById("add-new").addEventListener("click", test);​​​​​​​
+      
+// }
+
+// function test(){
+//   console.log("det virker"); 
+//         const data = {​​​​​​​
+//             fullname: "Anton Hansen",
+//             gamertag: "anton_anden",
+//             email: "anton@anden.dk",
+//             date_of_birth: "1994/02/03",
+//             password: "453218654565",
+//             game_type: "FPS",
+//             game_title: "CS",
+//             game_hours: "8",
+//             game_time_of_day: "10:00am - 14:00pm",
+//             improve_areas: ["sleep"],
+//             interested_areas: "stress",
+//           }​​​​​​​;
+//         post(data);
+// }
+
+
+
+
+// // window.addEventListener("load", (e) =>{​​​​​​​
+// //   console.log("hul igennem"); 
+
+// // }​​​​​​​); 
+
+
+
+// function post(data){
+//     const postData = JSON.stringify(data);
+//     fetch(endpoint, {
+//       method: "post",
+//       headers: {
+//         "Content-Type": "application/json; charset=utf-8",
+//         "x-apikey": apiKey,
+//         "cache-control": "no-cache"
+//       },
+//       body: postData
+//     })
+//       .then((res) => res.json())
+//       .then((data) => showHero(data));
+//   }
+
+
+
+
+// /* 
+// window.addEventListener("load", post(data));
+
+// post(data);
+
+// function post(data) {
+//   const data = {
+//     fullname: "Anton Hansen",
+//     gamertag: "anton_anden",
+//     email: "anton@anden.dk",
+//     date_of_birth: "1994/02/03",
+//     password: "453218654565",
+//     game_type: "FPS",
+//     game_title: "CS",
+//     game_hours: "8",
+//     game_time_of_day: "10:00am - 14:00pm",
+//     improve_areas: "sleep",
+//     interested_areas: "stress",
+//   };
+
+//   const postData = JSON.stringify(data);
+//   fetch("https://frontend2021-7beb.restdb.io/rest/ezone", {
+//     method: "post",
+//     headers: headers,
+//     body: postData,
+//   })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// }
+//  */
