9a54ea486a94997def47b7688cc305dfee91e378
vivimortensen <vivi4531@stud.kea.dk>
2021-04-26 09:57:42 +0200

    2604 post function works + vite build works

--- main.js
+++ main.js
@@ -6,17 +6,20 @@ window.addEventListener("DOMContentLoaded", init);
 
 const endpoint = "https://frontend2021-7beb.restdb.io/rest/ezone"; 
 const apiKey = "60740be1f592f7113340f013";
-const lort = {​​​​​​​fullname: "Anton Hansen",
-              gamertag: "anton_anden",
-              email: "anton@anden.dk",
-              date_of_birth: "1994/02/03",
-              password: "453218654565",
-              game_type: "FPS",
-              game_title: "CS",
-              game_hours: "8",
-              game_time_of_day: "10:00am - 14:00pm",
-              // improve_areas: ["sleep"],
-              interested_areas: "stress"}​​​​​​​;
+const lort = {
+  fullname: "Vivi",
+  gamertag: "Vivse" + Math.random(),
+  email: "anton@anden.dk",
+  date_of_birth: "1993-05-28", 
+  password: "dfsdf", 
+  game_type: ["FPS"],
+  game_title: ["sdsdtseg"],
+  game_hours: "8",
+  game_time_of_day: ["10:00am - 14:00pm"],
+  improve_areas: ["sleep"],
+  interested_areas: ["stress"]
+};
+
 
 
 
@@ -35,6 +38,7 @@ function post(data){
   "x-apikey" : apiKey, 
 "cache-control" : "no-cache", 
     },
+    body: postData
   })
   .then((res) => res.json())
   .then((data) => console.log(data));  
