85edb7fa73f43996287076acfc76306406485547
vivimortensen <vivi4531@stud.kea.dk>
2021-04-27 16:57:47 +0200

    2704 form popup working + post function working

--- main.js
+++ main.js
@@ -7,14 +7,27 @@ import { headers, endpoint } from "./settings.js";
 
 window.addEventListener("DOMContentLoaded", init); 
 
+const popup = document.querySelector("#popup"); 
 
 function init(){
   console.log("hul"); 
+
+// document.querySelector(".frontpage button").addEventListener("click", showPopup);
+document.querySelector("#frontpagebutton").addEventListener("click", () => popup.style.visibility = "visible"); 
+
+//Luk popup
+document.querySelector(".close-icon").addEventListener("click", () => popup.style.visibility = "hidden");
 
 //FORM (knapper)
 document.querySelector("#knap1").addEventListener("click", nextTab); 
 
 }
+
+// function showPopup(){
+//   document.querySelector("#popup").addEventListener("click", () => popup.style.display = "none"); 
+// }
+
+
 
 function nextTab(){
   document.querySelector(".fieldset1").classList.add("hide"); 
@@ -25,7 +38,6 @@ document.querySelector("#knap2").addEventListener("click", lastTab);
 function lastTab(){
   document.querySelector(".fieldset2").classList.add("hide"); 
   document.querySelector(".fieldset3").classList.remove("hide"); 
-  document.querySelector("#knap3").addEventListener("click", post); 
 }
 
 
@@ -36,146 +48,45 @@ function post(data){
   fetch(endpoint, {
     method : "POST", 
     headers : headers,
-    body: postData
+    body: postData,
   })
   .then((res) => res.json())
   .then((data) => console.log(data));  
 }
 
 const form = document.querySelector("form"); 
+
+const formData = new FormData(form); 
+
+const formFields = Object.fromEntries(formData.entries()); 
+
+// console.log(formFields); 
+
+function sendData(){
+  post({
+    fullname: form.elements.fullname.value, 
+    gamertag: form.elements.gamertag.value,
+    profile_img: form.elements.profileimage.value, 
+    email: form.elements.email.value,
+    date_of_birth: form.elements.dob.value, 
+    phonenumber: form.elements.phonenumber.value,
+    password: form.elements.password.value, 
+    // console: form.elements.console.value, 
+    // game_type: form.elements.gametype.value,
+    // game_title: form.elements.gametitle.value,
+    // game_level: form.elements.gamelevel.value,
+    game_hours: form.elements.hours.value,
+    // game_time_of_day: form.elements.gametod.value,
+    // improve_areas: form.elements.improvements.value,
+    // interested_areas: form.elements.learning.value, 
+    comment: form.elements.comments.value
+  }); 
+  
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-post({
-  fullname: form.elements.fullname.value, 
-  gamertag: "Vivse",
-  profile_img: "profile_img.jpg", 
-  email: "anton@anden.dk",
-  date_of_birth: "1993-05-28", 
-  phonenumber: "27570612",
-  password: "dfsdf", 
-  console: ["xbox, pc"], 
-  game_type: ["FPS"],
-  game_title: ["sdsdtseg"],
-  game_level: "Beginner",
-  game_hours: "8",
-  game_time_of_day: ["10:00am - 14:00pm"],
-  improve_areas: ["sleep"],
-  interested_areas: ["stress"], 
-  comment: "text comment bla bla bla"
-}); 
-
-// const data = {
-//   fullname: form.elements.fullname.value, 
-//   gamertag: "Vivse",
-//   profile_img: "profile_img.jpg", 
-//   email: "anton@anden.dk",
-//   date_of_birth: "1993-05-28", 
-//   phonenumber: "27570612",
-//   password: "dfsdf", 
-//   console: ["xbox, pc"], 
-//   game_type: ["FPS"],
-//   game_title: ["sdsdtseg"],
-//   game_level: "Beginner",
-//   game_hours: "8",
-//   game_time_of_day: ["10:00am - 14:00pm"],
-//   improve_areas: ["sleep"],
-//   interested_areas: ["stress"], 
-//   comment: "text comment bla bla bla"
-// };
+  sendData(); 
 })
-
-
-
-// /* import { headers } from "./settings";
-//  */
-
-//window.addEventListener("DOMContentLoaded", start); 
-
-// function start(){
-//   console.log("hallo");
-
-
-//     document.getElementById("add-new").addEventListener("click", test);​​​​​​​
-      
-// }
-
-// function test(){
-//   console.log("det virker"); 
-//         const data = {​​​​​​​
-//             fullname: "Anton Hansen",
-//             gamertag: "anton_anden",
-//             email: "anton@anden.dk",
-//             date_of_birth: "1994/02/03",
-//             password: "453218654565",
-//             game_type: "FPS",
-//             game_title: "CS",
-//             game_hours: "8",
-//             game_time_of_day: "10:00am - 14:00pm",
-//             improve_areas: ["sleep"],
-//             interested_areas: "stress",
-//           }​​​​​​​;
-//         post(data);
-// }
-
-
-
-
-// // window.addEventListener("load", (e) =>{​​​​​​​
-// //   console.log("hul igennem"); 
-
-// // }​​​​​​​); 
-
-
-
-// function post(data){
-//     const postData = JSON.stringify(data);
-//     fetch(endpoint, {
-//       method: "post",
-//       headers: {
-//         "Content-Type": "application/json; charset=utf-8",
-//         "x-apikey": apiKey,
-//         "cache-control": "no-cache"
-//       },
-//       body: postData
-//     })
-//       .then((res) => res.json())
-//       .then((data) => showHero(data));
-//   }
-
-
-
-
-// /* 
-// window.addEventListener("load", post(data));
-
-// post(data);
-
-// function post(data) {
-//   const data = {
-//     fullname: "Anton Hansen",
-//     gamertag: "anton_anden",
-//     email: "anton@anden.dk",
-//     date_of_birth: "1994/02/03",
-//     password: "453218654565",
-//     game_type: "FPS",
-//     game_title: "CS",
-//     game_hours: "8",
-//     game_time_of_day: "10:00am - 14:00pm",
-//     improve_areas: "sleep",
-//     interested_areas: "stress",
-//   };
-
-//   const postData = JSON.stringify(data);
-//   fetch("https://frontend2021-7beb.restdb.io/rest/ezone", {
-//     method: "post",
-//     headers: headers,
-//     body: postData,
-//   })
-//     .then((res) => res.json())
-//     .then((data) => console.log(data));
-// }
-//  */
-
-
 
